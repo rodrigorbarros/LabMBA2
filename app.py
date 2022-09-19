185d70fbb3749fc3bc1dea2860b76568035c2a9d
Rodrigo Barros <31513133+rodrigorbarros@users.noreply.github.com>
2022-09-18 21:26:06 -0300

    added: dockerfile

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO v02"
+    return "Laboratório DevOps - FIAP 8ASO v03"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
