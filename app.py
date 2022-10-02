584af451e4bde77443efb548912b16545ffccac2
Rodrigo Barros <31513133+rodrigorbarros@users.noreply.github.com>
2022-10-02 20:11:22 -0300

    entrega final v2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO - RM342801 - Entrega Final"
+    return "Laboratório DevOps - FIAP 8ASO - RM342801 - Entrega Final v2"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
